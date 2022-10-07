2210b4d38c8093af62093eb36e4dddaafaf59fcc
Battlehart <sarah.stelljes@gmail.com>
2022-10-07 09:52:47 -0400

    added front end react, edited server in back end, and created a package.json in root folder

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
@@ -23,6 +25,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
 
   server.applyMiddleware({ app });
+
+  if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
